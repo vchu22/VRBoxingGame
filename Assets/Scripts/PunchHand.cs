18c234af4b1b22364d3a62feae1530cfd4486b06
Vanessa Chu <10492258+vchu22@users.noreply.github.com>
2020-10-18 21:04:34 -0400

    added hand positions tracking

---
+++ Assets/Scripts/PunchHand.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class PunchHand : MonoBehaviour
+{
+    public GameObject hand;
+    private Rigidbody rBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // hand = GameObject.Find("");
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rBody.MovePosition(hand.transform.position);
+        rBody.MoveRotation(hand.transform.rotation);
+    }
+}
